5e00922529acb33cc8efd14c122fc853302d962d
Quazia <arthur@neynar.com>
2025-05-09 16:23:39 -0400

    feat: basic signature flow functional

--- src/privy-712-farcaster-verification/privy-test.ts
+++ src/privy-712-farcaster-verification/privy-test.ts
@@ -1,46 +1,58 @@
-import { PrivyClient } from '@privy-io/js-sdk-core'
+import { config } from 'dotenv'
+config()
+// @ts-ignore
+import {PrivyClient} from '@privy-io/server-auth';
 import { createWalletClient, createPublicClient, http, custom } from 'viem'
 import { optimism } from 'viem/chains'
 import { eip712 } from '@farcaster/hub-web'
 
 const FID = 1234
-const privy = new PrivyClient({
-  appId: 'your-app-id',
-  loginMethods: ['wallet', 'email'], // or just 'wallet' if using embedded
-  embeddedWallets: { createOnLogin: true },
-})
 
+const privy = new PrivyClient(
+  process.env.PRIVY_ID!,
+  process.env.PRIVY_SECRET!,
+  // {
+  //   walletApi: {
+  //     authorizationPrivateKey: process.env.PRIVY_AUTH_PRIVATE_KEY!
+  //   }
+  // }
+)
+
+// Here's the section of code where they're running into issues
+// https://github.com/neynarxyz/hub-monorepo/blob/472df705c3e4ac841e015ba877feb7cfa3f911dc/packages/core/src/validations.ts#L490-L529
+// We're doing a contract signature verification so it needs to be verificationType = 1
+// https://github.com/neynarxyz/hub-monorepo/blob/472df705c3e4ac841e015ba877feb7cfa3f911dc/packages/core/src/crypto/eip712.ts#L137
+// T
 async function main() {
-  await privy.login()
+  const {id, address, chainType} = await privy.walletApi.create({chainType: 'ethereum'});
 
-  const user = await privy.getUser()
-  const wallet = user.wallet
+  if (!address) throw new Error('No embedded wallet found')
 
-  if (!wallet) throw new Error('No embedded wallet found')
+  const {signature, encoding} = await privy.walletApi.ethereum.signMessage({
+  walletId: id,
+  message: 'Hello server wallets!'
+});
 
-  const provider = await wallet.getEthereumProvider()
-  const publicClient = createPublicClient({ chain: optimism, transport: http() })
 
-  const block = await publicClient.getBlock()
-  const message = {
-    domain: eip712.EIP_712_FARCASTER_DOMAIN,
-    types: { VerificationClaim: eip712.EIP_712_FARCASTER_VERIFICATION_CLAIM },
-    primaryType: 'VerificationClaim',
-    message: {
-      fid: FID,
-      address: wallet.address,
-      blockHash: block.hash,
-      network: 1,
-    },
-    account: wallet.address,
-  }
+  // const block = await publicClient.getBlock()
+  // const message = {
+  //   domain: eip712.EIP_712_FARCASTER_DOMAIN,
+  //   types: { VerificationClaim: eip712.EIP_712_FARCASTER_VERIFICATION_CLAIM },
+  //   primaryType: 'VerificationClaim',
+  //   message: {
+  //     fid: FID,
+  //     address: wallet.address,
+  //     blockHash: block.hash,
+  //     network: 1,
+  //   },
+  //   account: wallet.address,
+  // }
 
-  const walletClient = createWalletClient({ chain: optimism, transport: custom(provider) })
-  const signature = await walletClient.signTypedData(message)
+  // const walletClient = createWalletClient({ chain: optimism, transport: custom(provider) })
 
   console.log('Signature:', signature)
-  console.log('Wallet address:', wallet.address)
-  console.log('Block hash:', block.hash)
+  console.log('Wallet address:', address)
+  //console.log('Block hash:', block.hash)
 }
 
 main().catch(console.error)
