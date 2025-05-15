4b0b03f2ac290226f70c5f0188a1beb737ea68c6
Quazia <arthur@neynar.com>
2025-05-15 18:38:17 -0400

    feat: verification working locally

--- src/privy-712-farcaster-verification/privy-test.ts
+++ src/privy-712-farcaster-verification/privy-test.ts
@@ -2,7 +2,7 @@ import { config } from 'dotenv'
 config()
 // @ts-ignore
 import {PrivyClient} from '@privy-io/server-auth';
-import {  createPublicClient, http } from 'viem'
+import { Address, createPublicClient, hashTypedData, http } from 'viem'
 import { optimism } from 'viem/chains'
 import { eip712 } from '@farcaster/hub-web'
 import {
@@ -12,19 +12,26 @@ import {
 
 
 // Idk if this needs to be a bigint or not
-const FID = 1234;
-  const publicClient = createPublicClient({ chain: optimism, transport: http() })
+const FID = 14206;
+const publicClient = createPublicClient({ chain: optimism, transport: http() })
 
-const getMessageToSign = async (address: `0x${string}`, account: `0x${string}`) => {
-    const latestBlock = await publicClient.getBlock()
+const getMessageToSign = async (address: `0x${string}`, blockHash?: `0x${string}`) => {
+    const latestBlockHash = blockHash ? blockHash : (await publicClient.getBlock()).hash
     // Message composition information
     // https://github.com/farcasterxyz/hub-monorepo/blob/733894b77427b71f5cbc4d563cd54e17c37ab2a6/packages/core/src/crypto/eip712.ts#L16
     return {
-      domain: eip712.EIP_712_FARCASTER_DOMAIN,
-      types: { VerificationClaim: eip712.EIP_712_FARCASTER_VERIFICATION_CLAIM },
+      domain: { ...eip712.EIP_712_FARCASTER_DOMAIN, chainId: optimism.id },
+      types: {
+        EIP712Domain: [
+          { name: 'name', type: 'string' },
+          { name: 'version', type: 'string' },
+          { name: 'salt', type: 'bytes32' },
+          { name: 'chainId', type: 'uint256' },
+        ],
+        VerificationClaim: eip712.EIP_712_FARCASTER_VERIFICATION_CLAIM,
+      },
       primaryType: 'VerificationClaim',
-      message: { fid: FID, address, blockHash: latestBlock.hash, network: 10 },
-      account,
+      message: { fid: FID, address, blockHash: latestBlockHash, network: FarcasterNetwork.MAINNET, protocol: 0 },
     }
     /*
       verifyTypedData({
@@ -41,11 +48,11 @@ const getMessageToSign = async (address: `0x${string}`, account: `0x${string}`) 
 const privy = new PrivyClient(
   process.env.PRIVY_ID!,
   process.env.PRIVY_SECRET!,
-  // {
-  //   walletApi: {
-  //     authorizationPrivateKey: process.env.PRIVY_AUTH_PRIVATE_KEY!
-  //   }
-  // }
+  {
+    walletApi: {
+      authorizationPrivateKey: process.env.PRIVY_AUTH_PRIVATE_KEY!
+    }
+  }
 )
 
 // Here's the section of code where they're running into issues
@@ -54,89 +61,88 @@ const privy = new PrivyClient(
 // https://github.com/neynarxyz/hub-monorepo/blob/472df705c3e4ac841e015ba877feb7cfa3f911dc/packages/core/src/crypto/eip712.ts#L137
 // T
 async function main() {
-  const {id, address, chainType} = await privy.walletApi.create({chainType: 'ethereum'});
+  const address = "0x3B1F5A8B542De444a22453cA8c6ac851360296Ce" as Address
+  const id = "i2ud7poom0dowqyfr893c5ue"
+  const publicClient = createPublicClient({ chain: optimism, transport: http() });
 
-  if (!address) throw new Error('No embedded wallet found')
+  const block = await publicClient.getBlock();
 
-  const {signature, encoding} = await privy.walletApi.ethereum.signMessage({
-  walletId: id,
-  message: 'Hello server wallets!'
-});
+  const messageToSign = await getMessageToSign(address as Address, block.hash as `0x${string}`)
+  console.log('Message to sign:', messageToSign)
+
+  const {signature, encoding} = await privy.walletApi.ethereum.signTypedData({
+    walletId: id,
+    typedData: messageToSign,
+  });
 
 
-  // const block = await publicClient.getBlock()
-  // https://github.com/farcasterxyz/hub-monorepo/blob/733894b77427b71f5cbc4d563cd54e17c37ab2a6/packages/core/src/crypto/eip712.ts#L80-L87
-  // const message = {
-  //   domain: eip712.EIP_712_FARCASTER_DOMAIN,
-  //   types: { VerificationClaim: eip712.EIP_712_FARCASTER_VERIFICATION_CLAIM },
-  //   primaryType: 'VerificationClaim',
-  //   message: {
-  //     fid: FID,
-  //     address: wallet.address,
-  //     blockHash: block.hash,
-  //     network: 1,
-  //   },
-  //   account: wallet.address,
-  // }
-   /*
-  https://github.com/farcasterxyz/hub-monorepo/blob/733894b77427b71f5cbc4d563cd54e17c37ab2a6/packages/core/src/verifications.ts#L9-L15
-
-    export type VerificationAddressClaimEthereum = {
-      fid: bigint;
-      address: `0x${string}`;
-      blockHash: `0x${string}`;
-      network: FarcasterNetwork;
-      protocol: Protocol.ETHEREUM; // This is an enum value equal to 0
-    };
-  */
-
-  // const walletClient = createWalletClient({ chain: optimism, transport: custom(provider) })
+ 
 
   console.log('Signature:', signature)
   console.log('Wallet address:', address)
   //console.log('Block hash:', block.hash)
 
   // --- begin Farcaster verification flow ---
-  const publicClient = createPublicClient({ chain: optimism, transport: http() });
-
-  const block = await publicClient.getBlock();
   const network = FarcasterNetwork.MAINNET;
   const blockHash = block.hash;
 
-
-  // What is the privy equivalent of this?
-  // const ethSignature = eip712Signer.signVerificationEthAddressClaim({
-  //   fid: FID,
-  //   address,
-  //   network,
-  //   blockHash,
-  // });
-
-  // if (ethSignature.isOk()) {
-  //   const message = await makeVerificationAddEthAddress(
-  //     { address, blockHash, ethSignature: ethSignature.value },
-  //     { fid: FID, network },
-  //     ed25519Signer
-  //   );
-  //   console.log('Verification message:', message);
-  // } else {
-  //   console.error('EIP712 signature failed', ethSignature.error);
-  // }
-  // --- end Farcaster flow ---
-
+  const verification1 = await publicClient.verifyTypedData({
+    address: address as Address,
+    domain: messageToSign.domain,
+    types: messageToSign.types,
+    primaryType: messageToSign.primaryType as 'VerificationClaim',
+    message: messageToSign.message,
+    signature: signature as `0x${string}`,
+  })
+  console.log("Verification result:", verification1)
 
   // Start Neynar Verification flow
-//   curl --request POST \
+    //   curl --request POST \
     //   --url https://api.neynar.com/v2/farcaster/user/verification \
     //   --header 'Content-Type: application/json' \
     //   --header 'x-api-key: NEYNAR_API_DOCS' \
     //   --data '{
-    //   "signer_uuid": "19d0c5fd-9b33-4a48-a0e2-bc7b0555baec",
+    //   "signer_uuid": "05d16904-8565-43d2-9a6c-a4c866a7d023",
     //   "address": "0x1ea99cbed57e4020314ba3fadd7c692d2de34d5f",
     //   "block_hash": "0x191905a9201170abb55f4c90a4cc968b44c1b71cdf3db2764b775c93e7e22b29",
     //   "eth_signature": "0x2fc09da1f4dcb723fefb91f77932c249c418c0af00c66ed92ee1f35002c80d6a1145280c9f361d207d28447f8f7463366840d3a9309036cf6954afd1fd331beb1b"
     // }'
+    const signer_uuid = process.env.ADDRESS_VALIDATION_SIGNER_UUID!;
 
-}
+    // When this comes back as false it gives us this: https://github.com/farcasterxyz/hub-monorepo/blob/733894b77427b71f5cbc4d563cd54e17c37ab2a6/packages/core/src/validations.ts#L524-L526
+    const verification2 = await publicClient.verifyTypedData({
+      address: address as Address,
+      domain: { ...eip712.EIP_712_FARCASTER_DOMAIN, chainId: optimism.id },
+      types: {
+      VerificationClaim: eip712.EIP_712_FARCASTER_VERIFICATION_CLAIM },
+      primaryType: "VerificationClaim",
+      message: { fid: FID, address, blockHash: block.hash, network: 1, protocol: 0 },
+      signature: signature as `0x${string}`,
+    })
+    console.log("Verification result:", verification2)
+  
+    // FID is pulled from the signer_uuid so that needs to sync up
+    const neynarResponse = await fetch(
+    'https://api.neynar.com/v2/farcaster/user/verification',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': process.env.NEYNAR_API_KEY!,
+      },
+      body: JSON.stringify({
+        signer_uuid,
+        address,
+        block_hash: blockHash,
+        eth_signature: signature,
+        verification_type: 1,
+        chain_id: 10,
+      }),
+    }
+  );
+  const neynarResult = await neynarResponse.json();
+  console.log('Neynar verification response:', neynarResult);
+
+} // end main
 
 main().catch(console.error)
